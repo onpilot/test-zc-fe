c0603c08379bfa3b30e70f459f341366e227db29
Idan <72357530+onpilot@users.noreply.github.com>
2022-06-29 23:34:31 +0700

    add eventlistener to toggle class of selected card

--- html-css/index.js
+++ html-css/index.js
@@ -1,3 +1,18 @@
-const button = document.querySelector('button');
-
+// Button click trigger alert
 const handleClick = () => alert('Success');
+
+// .card click toggle class
+const cards = document.querySelectorAll('.card');
+
+const toggleClass = (card) => {
+  // deselect other card instances with .selected class (if any)
+  const selected = document.querySelectorAll('.selected');
+  selected.forEach((e) => e.classList.toggle('selected'));
+
+  // add .selected class to currently selected card
+  card.classList.toggle('selected');
+};
+
+cards.forEach((card) =>
+  card.addEventListener('click', () => toggleClass(card))
+);
